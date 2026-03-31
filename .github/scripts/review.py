6d3e8890866649b08acd57965f6678146a7fed7d
Robin <ras23032@uconn.edu>
2026-03-31 19:46:30 -0400

    testing files

---
+++ .github/scripts/review.py
@@ -0,0 +1,50 @@
+import os
+import requests
+from groq import Groq
+
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+PR_NUMBER = os.environ["PR_NUMBER"]
+REPO = os.environ["REPO"]
+
+client = Groq(api_key = os.environ["GROQ_API_KEY"])
+
+def get_pr_diff():
+    url = f"https://api.github.com/repos{REPO}/pulls/{PR_NUMBER}"
+    headers = {
+        "Authorization":f"Bearer {GITHUB_TOKEN}",
+        "Accept":"application/vnd.github.v3.diff"
+
+    }
+    return requests.get(url,headers=headers).text
+
+def groq_review(diff):
+    prompt = f""" You are an expert code reviewer. Review this git diff:
+1.  **Summary** - what changed
+2.  **Issues** - bugs, security problems, bad practices
+3.  **Suggestions** - specific improvements with code examples
+4.  **Verdict** - Approve/Request changes/ Needs discussion
+
+Be consise and actionalbe. Diff:
+{diff[:8000]}
+            """
+    response = client.chat.competions.create(
+        model = "llama-3.3-70b-versatile"
+        messages = [{"role":"user","content": prompt}]
+    )
+
+    return response.choices[0].message.content
+
+def post_comment(review):
+    url = f"https://api.github.com/repos/{REPO}/issues/{PR_NUMBER}/comments"
+    headers = {
+        "Authoriztion": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd/github.v3+json"
+
+    }
+    requests.post(url,headers=headers, json={"body":f'## AI Code Review \n\n{review}'})
+
+if __name__ == "__main__":
+    diff = get_pr_diff()
+    review = groq_review(diff)
+    post_comment(review)
+    print("review posted")
